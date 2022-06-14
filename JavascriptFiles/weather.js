79dee31faf4ab53d6adf882dd921e670cdc7ce33
Ebrima Saho <saho.ebrima@ymail.com>
2022-06-14 13:43:59 -0700

    Added response output for weather fetch

--- JavascriptFiles/weather.js
+++ JavascriptFiles/weather.js
@@ -18,3 +18,36 @@ function fetchWeather(zip) {
     .then((data) => console.log(data));
 }
 fetchWeather("75204");
+
+/*
+{
+  coord: { lon: -96.7851, lat: 32.8038 },
+  weather: [ { id: 721, main: 'Haze', description: 'haze', icon: '50d' } ],
+  base: 'stations',
+  main: {
+    temp: 309.09,
+    feels_like: 312.53,
+    temp_min: 308.09,
+    temp_max: 310.22,
+    pressure: 1012,
+    humidity: 41
+  },
+  visibility: 10000,
+  wind: { speed: 11.32, deg: 160, gust: 16.46 },
+  clouds: { all: 40 },
+  dt: 1655239300,
+  sys: {
+    type: 2,
+    id: 2018848,
+    country: 'US',
+    sunrise: 1655205518,
+    sunset: 1655256982
+  },
+  timezone: -18000,
+  id: 0,
+  name: 'Dallas',
+  cod: 200
+}
+
+
+*/
