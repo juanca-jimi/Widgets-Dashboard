bffa9e05b61821f9a4b70cb35aee48e395a10a26
JuanCarlos Jimenez <jcj237@outlook.com>
2022-06-14 09:06:57 -0400

    Adding dependency for Mongoose

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require("express");
+
+//Body-parser is a middleware. They help to tidy up the request object before we use them. Express lets us use middleware with the use method.
+const bodyParser= require('body-parser')
+
 const app = express();
+
+
+//Body parser must be placed before CRUD operations
+app.use(bodyParser.urlencoded({extended: true }))
+
 
 app.listen(3000, function () {
   console.log("listening on 3000");
