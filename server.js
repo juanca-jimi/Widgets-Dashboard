67492aa50dff98982daafd9c31809d2a27feb0a2
JuanCarlos Jimenez <jcj237@outlook.com>
2022-06-14 09:46:27 -0400

    Adding about.txt as comments for the whole models folder

--- server.js
+++ server.js
@@ -5,6 +5,32 @@ const bodyParser= require('body-parser')
 
 const app = express();
 
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const url = 'mongodb+srv://Team4:7vap3lwvmEM24GBm@widgets-dash.nlgo8ag.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.connect(url, { useNewUrlParser: true})
+
+const db = mongoose.connection
+
+
+const schema = new Schema({
+
+})
+
+//To test whether the connection has succeeded or not
+db.once('open', _ => {
+  console.log('Database connected:', url)
+})
+
+db.on('error', err => {
+  console.error('connection error:', err)
+})
+//-------------------------------------------------------
+
+
 
 //Body parser must be placed before CRUD operations
 app.use(bodyParser.urlencoded({extended: true }))
